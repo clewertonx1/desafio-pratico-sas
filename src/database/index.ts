9afe4ba91084ab5572989886f278a0a1054b2105
Clewerton Roque <clewerton203@gmail.com>
2021-12-14 18:51:12 -0300

    Change connection with database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,21 +1,20 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection, getConnectionOptions } from "typeorm";
 
 interface IOptions {
   host: string;
 }
 
-
 // Docker database
-// getConnectionOptions().then(options => {
-//   const newOptions = options as IOptions;
-//   newOptions.host = 'database';
-//   createConnection({
-//     ...options,
-//   });
-// });
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+  newOptions.host = "database";
+  createConnection({
+    ...options,
+  });
+});
 
 // Local database
-getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  createConnection(options)
-});
+// getConnectionOptions().then(options => {
+//   const newOptions = options as IOptions;
+//   createConnection(options)
+// });
